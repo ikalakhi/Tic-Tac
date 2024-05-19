e8c4cf55e32fe72b9e47eb628dd1f950338341a4
ikalakhi <imranekalakhi13@gmail.com>
2024-05-19 16:41:42 +0100

    fixing th edite button: using hooks

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+    const [isEditing, setIsEding] = useState(false);
+
+    let playerName = <span className="player-name">{name}</span>;
+    if(isEditing) {
+        playerName = <input type="text" required/>;
+    }
     return(
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edite</button>
+            <button onClick={() => setIsEding(true)}>Edite</button>
         </li>
     );
 }
