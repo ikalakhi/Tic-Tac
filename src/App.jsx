44eda916c125f7144686994a59ef24a356b848b6
ikalakhi <imranekalakhi13@gmail.com>
2024-06-03 22:21:47 +0100

    outputting logs

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handeSelectSquare} turns={gameTurns}/>
       </div>
-      <Log/>
+      <Log turns={gameTurns}/>
     </main>
   )
 }
